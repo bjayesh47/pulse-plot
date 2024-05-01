64acc3bdfed5db181e14841a68e2e4a474e25f66
bjayesh47 <bjayesh47@gmail.com>
2024-05-01 21:44:58 +0530

    correction

--- data_acquisition/config.py
+++ data_acquisition/config.py
@@ -1,5 +1,5 @@
-username = 'bjayesh47'
-client_id ='23c311b2386c40089322b262a50afa07'
-client_secret = 'e95366676e7b46fc9b0ae587627416ab'
+username = 'username'
+client_id ='client-id'
+client_secret = 'client-secret'
 redirect_uri = 'http://localhost:8000'
 scope = 'user-read-recently-played'
